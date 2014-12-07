55dae84abe4c7c5619e3fca25dc6f36cf560ad95
Mateus Machado Luna <mateus.m.luna@gmail.com>
2014-12-07 12:07:37 -0500

    Create world.h
    header for the world.cpp

---
+++ world.h
@@ -0,0 +1,18 @@
+#ifndef _WORLD_
+#define _WORLD_
+
+void lAmbient();
+
+void mRusty();
+
+void fog();
+
+void drawLines();
+
+void createSkybox();
+
+void drawGround();
+
+void drawIsengard();
+
+#endif
